02d3662113a5e216707df26d91556e3e45923814
pcreem <creempepper@gmail.com>
2020-12-27 05:58:33 +0000

    progress

--- Algorithms/SelectionSort.js
+++ Algorithms/SelectionSort.js
@@ -1,20 +1,15 @@
-function selectionSort(array) {
-  // Only change code below this line
-  const min = Math.min(...array)
-  const max = Math.max(...array)
-  let emptyarr = []
-  let number = min
-
-  while (array.length !== 0 && number <= max) {
-    while (array.indexOf(number) !== -1) {
-      emptyarr.push(number);
-      array.splice(array.indexOf(number), 1)
-    }
-    number++
-  }
-  return emptyarr;
-  // Only change code above this line
+function swap(a, b, arr) {
+  let tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
 }
-
-
-selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min] > array[j]) min = j;
+    }
+    swap(i, min, array);
+  }
+  return array;
+}
